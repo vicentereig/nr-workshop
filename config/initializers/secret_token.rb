2415e42d5605dc2cf651acefcbaa4c97a19f8a36
Vicente Reig Rincon de Arellano <vicente@newrelic.com>
2014-02-05 16:27:22 -0800

    rails new nr-workshop -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NrWorkshop::Application.config.secret_key_base = 'f1e49bb5b862cd95196d997aded11ae5ef0cde2df29729f3812d757f355af008cc2b37c0b60cb2435774876d54c3347b5fe3461344470ecda5f3503034800e01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NrWorkshop::Application.config.session_store :cookie_store, key: '_nr-workshop_session'
