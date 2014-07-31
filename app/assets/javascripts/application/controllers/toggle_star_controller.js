2f0c811b71c207bfb702252f077d0ffffec165cd
Vicente Reig Rincon de Arellano <vicente@newrelic.com>
2014-07-31 08:59:49 -0700

    Moving the ToggleStarController where it belongs: the directive.

--- app/assets/javascripts/application/controllers/toggle_star_controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('ToggleStarController', ['$scope', 'TrackRepo', function($scope, trackRepo){
-    $scope.toggleStar = function() {
-        $scope.track.starred = !$scope.track.starred;
-        trackRepo.save($scope.track);
-    }
-}])
--- app/assets/javascripts/application/directives/starTrackComponent/directive.js
+++ app/assets/javascripts/application/directives/starTrackComponent/directive.js
@@ -1,10 +1,19 @@
+app.controller('ToggleStarController', ['$scope', 'TrackRepo', function($scope, trackRepo){
+    $scope.toggleStar = function() {
+        $scope.track.starred = !$scope.track.starred;
+        trackRepo.save($scope.track);
+    }
+}])
+
+
 function StarTrackComponent(){
     return {
         restrict: 'E',
         templateUrl: 'application/directives/starTrackComponent/index.html',
         scope: {
             track: '='
-        }
+        },
+        controller: 'ToggleStarController'
     }
 }
 
